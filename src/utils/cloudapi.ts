d98a6d2d531f5797779d06475434f43b7d2594d6
loheagn <loheagn@icloud.com>
2022-10-08 13:57:13 +0800

    Add global error handler for axios of cloudapi-client
    Signed-off-by: loheagn <loheagn@icloud.com>

--- src/utils/cloudapi.ts
+++ src/utils/cloudapi.ts
@@ -1,5 +1,19 @@
 import {Configuration, DefaultApiFactory} from "../cloudapi-client";
 import {getToken} from "./token";
+import globalAxios from "axios";
+import {message} from "antd";
+
+const cloudapiAxios = globalAxios
+
+cloudapiAxios.interceptors.response.use((response) => response, (error) => {
+    const statusCode = error.response?.status;
+    if (statusCode === 401 || statusCode === 403) {
+        message.error("登录已过期，请重新登录").then(r => r, e => {
+            throw e
+        });
+    }
+    throw error
+});
 
 export const cloudapiClient =
     DefaultApiFactory(new Configuration({
@@ -9,4 +23,4 @@ export const cloudapiClient =
             }
             return "";
         }
-    }))
+    }), undefined, cloudapiAxios);
