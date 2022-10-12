fbb43c8aa00c9dea4c5c15bef81db76625cb181b
loheagn <loheagn@icloud.com>
2022-10-12 12:42:44 +0800

    Wrap Images component using Card
    Signed-off-by: loheagn <loheagn@icloud.com>

--- src/components/image/Image.tsx
+++ src/components/image/Image.tsx
@@ -1,13 +1,24 @@
 import {ImageListTable} from "./ImageListTable";
 import {useParams} from "react-router-dom";
 import {ImageBuildTaskListTable} from "./ImageBuildTaskListTable";
+import {Card, Image} from "antd";
+import dockerSvg from '../../assets/docker.svg'
+
 
 export const Images = () => {
     const projectId = useParams().projectId ?? '0'
     return (
-        <div>
+        <Card title={
+            (
+                <>
+                    <h3>
+                        <Image src={dockerSvg} width={40}/> &nbsp;镜像服务
+                    </h3>
+                </>
+            )
+        }>
             <ImageListTable projectId={projectId}/>
             <ImageBuildTaskListTable projectId={projectId}/>
-        </div>
+        </Card>
     )
 }
