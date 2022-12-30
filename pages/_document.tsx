e99f222dde0acb5743719af18537f6759a4bd4fa
loheagn <loheagn@icloud.com>
2022-12-30 17:15:23 +0800

    Add polyfill for es2020 es2021 es2022
    Signed-off-by: loheagn <loheagn@icloud.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script src="/view/v2/es2020_es2021_es2022.polyfill.min.js" strategy='beforeInteractive' />
+      </Head>
       <body>
         <Main />
         <NextScript />
