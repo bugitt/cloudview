846bb486fab1399872710da335c932dcdcd46d5a
loheagn <loheagn@icloud.com>
2022-12-30 19:59:32 +0800

    Modify the location of cloudapi-client
    Signed-off-by: loheagn <loheagn@icloud.com>

--- tools/api-generate/cloudapi-client-generate.sh
+++ tools/api-generate/cloudapi-client-generate.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-rm -rf ../../src/cloudapi-client
-mkdir -p ../../src/cloudapi-client
+rm -rf ../../lib/cloudapi-client
+mkdir -p ../../lib/cloudapi-client
 openapi-generator generate -g typescript-axios -i https://raw.githubusercontent.com/bugitt/cloudapi/main/openapi/cloudapi_v2.yaml -o ./tmp
-cp ./tmp/*.ts ../../src/cloudapi-client/
+cp ./tmp/*.ts ../../lib/cloudapi-client/
 rm -rf ./tmp
