f3fd23adb79b169c4b29222271f45a869c21c87d
loheagn <loheagn@icloud.com>
2023-01-04 16:05:52 +0800

    Add k8s client
    Signed-off-by: loheagn <loheagn@icloud.com>

---
+++ lib/kube/client.ts
@@ -0,0 +1,28 @@
+import * as k8s from '@kubernetes/client-node';
+import { businessK8s } from '../config/env';
+
+const cluster = {
+    name: 'business-cluster',
+    server: businessK8s.server,
+};
+
+const user = {
+    name: 'business-rke',
+    token: businessK8s.userToken,
+};
+
+const context = {
+    name: 'business-rke',
+    user: user.name,
+    cluster: cluster.name,
+};
+
+const kc = new k8s.KubeConfig();
+kc.loadFromOptions({
+    clusters: [cluster],
+    users: [user],
+    contexts: [context],
+    currentContext: context.name,
+});
+
+export const k8sCoreV1Api = kc.makeApiClient(k8s.CoreV1Api);
