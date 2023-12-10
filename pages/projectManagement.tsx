f7105c4bf2650878c8d1eaf60c77195e75a527a2
loheagn <loheagn@icloud.com>
2023-12-10 18:26:47 +0800

    隐藏workflow相关界面的文档提示信息
    Signed-off-by: loheagn <loheagn@icloud.com>

--- pages/projectManagement.tsx
+++ pages/projectManagement.tsx
@@ -33,7 +33,7 @@ export default function Projects(props: InferGetServerSidePropsType<typeof getSe
                         <ResetPaaSTokenButton />
                     </ProDescriptions.Item>
                 </ProDescriptions>
-                <Alert
+                {/* <Alert
                     message={
                         <Typography>
                             请访问 <Typography.Link href='https://scs.buaa.edu.cn/doc/01_common/paas/' target='_blank'>文档</Typography.Link> 来了解我们新推出的PaaS工作流功能！
@@ -41,7 +41,7 @@ export default function Projects(props: InferGetServerSidePropsType<typeof getSe
                     }
                     type='info'
                     closable
-                />
+                /> */}
                 <ProjectTable projectList={projectList} />
             </Space>
         </>
